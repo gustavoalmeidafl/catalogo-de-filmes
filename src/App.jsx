3a66c396b1b4cd1e3673dd3b1237041ceea9124a
Gustavo <gugagualfort@gmail.com>
2025-09-03 22:51:00 -0300

    login-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "primeflex/primeflex.css"
 import "primeicons/primeicons.css"
 import "primereact/resources/themes/lara-light-blue/theme.css"
 import Paths from "./routes/paths";
+import "tailwindcss";
 
 const App = () => {
   return (
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,23 +2,26 @@ import { InputText } from 'primereact/inputtext';
 import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
 import { Button } from 'primereact/button';
-        
+import "tailwindcss";
+import './login.css';
 
 const Login = () => {
     return (  
         <>
-            <div class='bg-primary'>
-                <form>
-                    <h3>Entre na sua conta</h3>
-                    <label htmlFor="email">E-mail</label>
-                   <InputText id='email' type='email' placeholder='Insira seu email' />
+            <div className='h-screen flex align-items-center justify-content-center'>
+                <form id='card'className='p-3'>
+                    <h3 className='text-center  text-4x1'>Entre na sua conta</h3>
+                    <label htmlFor="email" className='block uppercase font-bold text-sm mb-1'>E-mail</label>
+                   <InputText id='email' type='email' placeholder='Insira seu email' className='mb-3 w-full'/>
                 
-                    <label htmlFor="senha">Senha</label>
+                    <label htmlFor="senha" className='block uppercase font-bold text-sm mb-1'>Senha</label>
+                    <div className='mb-3'>
                     <IconField>
                         <InputIcon className="pi pi-eye"> </InputIcon>
-                        <InputText id='senha' placeholder='Insira sua senha'/>
+                        <InputText id='senha' placeholder='Insira sua senha' className='w-full'/>
                     </IconField>
-                    <Button label="Submit" />
+                    </div>
+                    <Button label="Submit" type='submit' className='w-full'/>
 
                 </form>
          </div>
